4d6c085836b16083c9992bfdede118f5306d4627
PabVillar <pablo.villar.m@ug.uchile.cl>
2019-08-21 22:19:29 -0400

    updated code

--- src/test-t1.c
+++ src/test-t1.c
@@ -2,7 +2,75 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "t1.h"
+//#include "t1.h"
+
+//Parte a
+
+typedef unsigned int uint;
+
+
+uint overwrite(uint res, int nbits, int pos, int val){
+
+    uint val_desp;
+
+    val_desp = val << ((sizeof(uint)<<3) - nbits - pos);
+
+    return res | val_desp;
+}
+
+uint comprimir(uint *a, int nbits){
+    //Concatenacion de todos los elementos en a truncados a nbits
+    //La cantidad de elementos de a es un k tal que k*nbits <= sizeof(uint)<<3
+    uint aux[sizeof(a)];
+
+    uint res = 0;
+    int k;
+    int i = 0;
+    for (k = nbits; k <= (sizeof(uint)<<3); k += nbits)
+    {
+        aux[i] = (a[i] << (sizeof(uint)<<3)-nbits) >> ((sizeof(uint)<<3)-nbits);
+        //printf("%d\n", aux[i]);
+        res = overwrite(res,nbits,k-nbits,aux[i]);
+        i++;
+    }
+
+
+    return res >> ((sizeof(uint)<<3) - (k-nbits));
+}
+
+//Parte b
+char *insercion(char *d, char *s){
+    int totalsize = strlen(d)+strlen(s)+1;
+    char *aux = malloc(totalsize);
+    aux = strcat(strcpy(aux,s),d);
+
+    return aux;
+}
+
+
+//Parte c
+void insertar (char *d, char *s){
+    char *aux1;
+    char temp;
+    int i = strlen(d);
+    int k = 1;
+    while (k <= i){
+        aux1 = d + i - 1;
+        temp = *aux1;
+        aux1 = d + i - 1 + strlen(s);
+        *aux1 = temp;
+        i--;
+
+    }
+    i = strlen(s);
+    while(k<=i){
+        aux1 = s + i -1;
+        temp = *aux1;
+        aux1 = d + i -1;
+        *aux1 = temp;
+        i--;
+    }
+}
 
 int main(int argc, char **argv) {
   // Parte a
